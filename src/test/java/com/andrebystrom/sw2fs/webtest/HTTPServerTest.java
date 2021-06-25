5ebaa702dfca09399b4c62e7bca7f92f6aebebcc
André <andre.bystroem@outlook.com>
2021-06-25 10:04:07 +0200

    Started refactor branch to make code more testable

--- src/test/java/com/andrebystrom/sw2fs/webtest/HTTPServerTest.java
+++ src/test/java/com/andrebystrom/sw2fs/webtest/HTTPServerTest.java
@@ -2,30 +2,8 @@ package com.andrebystrom.sw2fs.webtest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import com.andrebystrom.sw2fs.web.HTTPServer;
 import org.junit.jupiter.api.Test;
-
-import java.io.File;
 
 public class HTTPServerTest
 {
-    @Test
-    void shouldThrowWhenRootIsNull()
-    {
-        assertThrows(IllegalArgumentException.class, () -> new HTTPServer(null));
-    }
-
-    @Test
-    void shouldThrowWhenRootIsNotADirectory()
-    {
-        File f = new File("");
-        assertThrows(IllegalArgumentException.class, () -> new HTTPServer(f));
-    }
-
-    @Test
-    void shouldNotThrowWhenRootIsValid()
-    {
-        File f = new File(".");
-        assertDoesNotThrow(() -> new HTTPServer(f));
-    }
 }
