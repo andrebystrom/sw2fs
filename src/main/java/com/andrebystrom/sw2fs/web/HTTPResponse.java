1d1448f31989bc4bedfdbcdcdb4d097cc759b288
André <andre.bystroem@outlook.com>
2021-07-14 19:49:02 +0200

    Added Javadoc for HTTPResponse

--- src/main/java/com/andrebystrom/sw2fs/web/HTTPResponse.java
+++ src/main/java/com/andrebystrom/sw2fs/web/HTTPResponse.java
@@ -4,6 +4,9 @@ import com.andrebystrom.sw2fs.web.interfaces.Response;
 
 import java.util.HashMap;
 
+/**
+ * Class representing an HTTP response.
+ */
 public class HTTPResponse implements Response
 {
     private String version;
@@ -12,59 +15,109 @@ public class HTTPResponse implements Response
     private String body;
     private static final String NEW_LINE = "\r\n";
 
+    /**
+     * Constructs a new HTTP response.
+     */
     public HTTPResponse()
     {
         this.headers = new HashMap<>();
     }
 
+    /**
+     * Set the HTTP response version.
+     *
+     * @param version the version to set to.
+     */
     @Override
     public void setVersion(String version)
     {
         this.version = version;
     }
 
+    /**
+     * Get the HTTP response version.
+     *
+     * @return the HTTP response version.
+     */
     @Override
     public String getVersion()
     {
         return this.version;
     }
 
+    /**
+     * Set the HTTP response status.
+     *
+     * @param status the status to set to.
+     */
     @Override
     public void setStatus(HTTPResponseStatus status)
     {
         this.status = status;
     }
 
+    /**
+     * Get the HTTP response status.
+     *
+     * @return the HTTP response status.
+     */
     @Override
     public HTTPResponseStatus getStatus()
     {
         return this.status;
     }
 
+    /**
+     * Add a HTTP response header.
+     *
+     * @param name the name of the header.
+     * @param val  the value of the header.
+     */
     @Override
     public void addHeader(String name, String val)
     {
         this.headers.put(name, val);
     }
 
+    /**
+     * Get the value of a HTTP response header.
+     *
+     * @param name the name of the header.
+     * @return the value of the header or null if it doesn't exist.
+     */
     @Override
     public String getHeader(String name)
     {
         return this.headers.get(name);
     }
 
+    /**
+     * Set the HTTP response body.
+     *
+     * @param body the body to set.
+     */
     @Override
     public void setBody(String body)
     {
         this.body = body;
     }
 
+    /**
+     * Get the HTTP response body.
+     *
+     * @return the HTTP response body.
+     */
     @Override
     public String getBody()
     {
         return this.body;
     }
 
+    /**
+     * Get a String representation of the HTTP response that's a valid server response.
+     *
+     * @return a String representation of the HTTP response.
+     */
     @Override
     public String getResponseMessage()
     {
