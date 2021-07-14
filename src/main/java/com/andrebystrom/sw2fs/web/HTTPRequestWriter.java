b74d8a2f39617efb5057df3e5cfa7223cdde0fce
André <andre.bystroem@outlook.com>
2021-07-14 19:44:53 +0200

    Added Javadoc to the HTTPRequestWriter

--- src/main/java/com/andrebystrom/sw2fs/web/HTTPRequestWriter.java
+++ src/main/java/com/andrebystrom/sw2fs/web/HTTPRequestWriter.java
@@ -8,8 +8,18 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
+/**
+ * Writes an HTTP Response to an output stream.
+ */
 public class HTTPRequestWriter implements RequestWriter
 {
+    /**
+     * Writes the response to the output stream.
+     *
+     * @param response     the response to write.
+     * @param outputStream the stream to write to.
+     * @throws IOException if there's an issue writing the response.
+     */
     @Override
     public void write(Response response, OutputStream outputStream) throws IOException
     {
