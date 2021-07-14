f193cb9820d5e962266ac534b0b25ca5ca35d618
André <andre.bystroem@outlook.com>
2021-07-14 19:41:13 +0200

    Added Javadoc to HTTPRequestReader

--- src/main/java/com/andrebystrom/sw2fs/web/HTTPRequestReader.java
+++ src/main/java/com/andrebystrom/sw2fs/web/HTTPRequestReader.java
@@ -4,10 +4,20 @@ import com.andrebystrom.sw2fs.web.interfaces.RequestReader;
 
 import java.io.*;
 
+/**
+ * Responsible for reading a HTTP request from an input stream into a String.
+ */
 public class HTTPRequestReader implements RequestReader
 {
     private int bytesRead;
 
+    /**
+     * Reads the headers in the HTTP request.
+     *
+     * @param inputStream the input stream to read from.
+     * @return the headers of the request.
+     * @throws IOException if there's an issue reading the request.
+     */
     @Override
     public String readHeaders(InputStream inputStream) throws IOException
     {
@@ -25,6 +35,14 @@ public class HTTPRequestReader implements RequestReader
         return headers;
     }
 
+    /**
+     * Reads the body of an HTTP request. NOTE: This has to be called after readHeaders.
+     *
+     * @param inputStream the input stream to read from.
+     * @param size        the number of bytes to read.
+     * @return the body of the HTTP request.
+     * @throws IOException if there's an issue reading the request.
+     */
     @Override
     public String readBody(InputStream inputStream, int size) throws IOException
     {
